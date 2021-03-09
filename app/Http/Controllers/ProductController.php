cfd0fa137e6b5cf0d59f97464b083056bb88990f
amirhosein <gooyacoder@gmail.com>
2021-03-09 11:07:02 +0330

    Upto 06_01

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
     public function index(){
+
+        //$products = DB::table('products')->get();
+        $products = Product::all();
+
+        dd($products);
 
         return view('products.index');
     }
@@ -20,6 +27,9 @@ class ProductController extends Controller
     }
 
     public function show($product){
+        // $result = DB::table('products')->find($product);
+        $result = Product::findOrFail($product);
+        dd($result);
         return view('products.show');
     }
 
